555450daa7f23680339e99b05a33455c599c9cd7
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-05-19 20:03:53 -0500

    Update Screenshot tool snipet.cs
    Second version, without testing, uploading the tested version tomorrow.

--- Screenshot tool snipet.cs
+++ Screenshot tool snipet.cs
@@ -1,36 +1,50 @@
 private void Take_Screenshot(int timeMS)
 {
-	//this.radioButton1.Checked = false;
+	//Waits a specific time in miliseconds, depending on your implementation you could multiply 
+	//the value passed to the function by 1000.
 	System.Threading.Thread.Sleep(timeMS);
 
 	Bitmap memoryImage;
+	long screenWidth = 0;
 
+	/************These flags are to be set normally with checkboxes from the GUI*****************/
+	bool primary = true;
+	bool clipboard = true;
+	bool taskBar = false;
+	/************************************************************************/
 	
-	if (prim)
-	{
-		memoryImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.WorkingArea.Height);
-		Size s = new Size(memoryImage.Width, memoryImage.Height);
-		Graphics memoryGraphics = Graphics.FromImage(memoryImage);
-		memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
-	}
+	if (primary) 
+		memoryImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, //if taskbar flag is set we include the taskbar in the screenshot
+	(taskbar ? Screen.PrimaryScreen.Bounds.Height : Screen.PrimaryScreen.WorkingArea.Height) );
 	else
-	{
-
+	{	
+		//We take both screens instead just the primary one.
 		memoryImage = new Bitmap(Screen.AllScreens[1].Bounds.Width, Screen.AllScreens[1].WorkingArea.Height);
-		Size s = new Size(memoryImage.Width, memoryImage.Height);
-		Graphics memoryGraphics = Graphics.FromImage(memoryImage);
-		memoryGraphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.Width, 0, 0, 0, s);
+		//If it's the secondary screen the one we want, we asign to the variable "screenWidth" the width of
+		//the primary screen, this way when we use "CopyFromScreen" we'll start from the end of the primary screen
+		screenWidth = Screen.PrimaryScreen.Bounds.Width;
 	}
-
+	
+	Size s = new Size(memoryImage.Width, memoryImage.Height);
+	Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+	//Coordinates sourceX, SourceY, DestinationX, DestinationY, Size
+	memoryGraphics.CopyFromScreen(screenWidth, 0, 0, 0, s);
+	
+	if(clipboard)//if clipboard flag is set we copy it to clipboard
+		System.Windows.Forms.Clipboard.SetImage(memoryImage);
+	
 	string str = "";
-
+	//We create the folder if it doesn't exist *****************************************
 	String filePath = string.Format(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
-	 @"\Screenshots\" + DateTime.Now.ToString("dd-MM-yy") + "\\");
+	 @"\Confidential\Screenshots\" + DateTime.Now.ToString("dd-MM-yy") + "\\");
+	 
 	System.IO.FileInfo file = new System.IO.FileInfo(filePath);
 	file.Directory.Create();
+	/********************************************************/
+	//We asign a name to the screenshot we're going to save.
 	str = string.Format(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
-	 @"\Screenshots\" + DateTime.Now.ToString("dd-MM-yy") + "\\" + DateTime.Now.ToString("dd-MM-yy") + " " + DateTime.Now.ToString("[hh-mm-ss]") + ".png");
-
+	 @"\Confidential\Screenshots\" + DateTime.Now.ToString("dd-MM-yy") + "\\" + DateTime.Now.ToString("dd-MM-yy") + " " + DateTime.Now.ToString("[hh-mm-ss]") + ".png");
+	
+	//We finally save the screnshot
 	memoryImage.Save(str);
-	this.radioButton1.Checked = true;
 }
